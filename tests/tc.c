17b9993e35850fee25e8871bd0acef4f90238621
Linh chi NGUYEN <lnguyen@e2r9p1.42.fr>
2018-08-11 18:30:22 +0200

    impossible segfault

--- tests/tc.c
+++ tests/tc.c
@@ -6,8 +6,8 @@ int main()
     profile *attacker = NULL;
     profile **attackers = &attacker;
     *kings = create_profile('K', 1, 1);
-	*attackers = create_profile('P',2,2);
-	add_profile(attackers,'R',0,0);
-    printf("%d\n", check_mate(kings, attackers, 4));
+//	*attackers = create_profile('P',2,2);
+//	add_profile(attackers,'R',0,0);
+    printf("%d\n", check_mate(kings, attackers, 2));
     return 0;
 }
--- tests/ts.c
+++ tests/ts.c
@@ -5,15 +5,21 @@ int main()
     profile *attacker = NULL;
     profile **kings = &king;
     profile **attackers = &attacker;
-    scan("R...",0,kings,attackers);
-	scan(".K..",1,kings,attackers);
-	scan("..P.",2,kings, attackers);
-	scan("....",3,kings,attackers);
+    scan("..",0,kings,attackers);
+	scan(".K",1,kings,attackers);
+//	scan("..P.",2,kings, attackers);
+//	scan("....",3,kings,attackers);
 	print_profile(kings);
 	ft_putchar('\n');
 	print_profile(attackers);
 	ft_putchar('\n');
-	if (check_mate(kings, attackers, 4) == 1)
+
+	int k_x;
+	k_x = (*kings)->x;
+	write(1,"before check mate\n", 100);
+	if (k_x == 1)
+		k_x = k_x + 1 - 1;
+	if (check_mate(kings, attackers, 2) == 1)
 		write(1, "success", 50);
 	else
 		write(1, "fail", 50);
